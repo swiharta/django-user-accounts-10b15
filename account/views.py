f1976e28d80dea5341dfb0018cbcb58fb0cd536f
Andrew Swihart <swihart.andrew@gmail.com>
2013-10-24 10:53:52 -0400

    Undid changes which didn't help, need to include csrf token in original full loaded page, including in ajax view fragment doesn't seem sufficient

--- account/views.py
+++ account/views.py
@@ -25,23 +25,8 @@ from account.mixins import LoginRequiredMixin
 from account.models import SignupCode, EmailAddress, EmailConfirmation, Account, AccountDeletion
 from account.utils import default_redirect, user_display
 
-from django.utils import decorators
-from django.views.decorators import csrf
 
-class EnsureCsrfCookieMixin(object):
-    """
-    Mixin for Django class-based views which forces a view to send the CSRF cookie.
-
-    This is useful when using Ajax-based sites which do not have an HTML form with
-    a :tag:`csrf_token` that would cause the required CSRF cookie to be sent.
-    """
-
-    @decorators.method_decorator(csrf.ensure_csrf_cookie)
-    def dispatch(self, *args, **kwargs):
-        return super(EnsureCsrfCookieMixin, self).dispatch(*args, **kwargs)
-
-
-class BootstrapAjaxFormView(EnsureCsrfCookieMixin, FormView):
+class BootstrapAjaxFormView(FormView):
   '''
   Handles AJAX forms when used with bootstrap-ajax.js
   '''
